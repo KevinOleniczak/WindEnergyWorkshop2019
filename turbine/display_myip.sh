2dae54326ed789aa3389a321dd5ebabd8be561a4
Kevin Oleniczak <kevinoleniczak@gmail.com>
2019-11-24 19:31:22 -0600

    updated display program on boot to fetch the latest repo

--- turbine/display_myip.sh
+++ turbine/display_myip.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sleep 5
 cd /home/pi/WindEnergyWorkshop2019
-echo "git pull for WindEnergyWorkshop2019"
+echo "git fetch for WindEnergyWorkshop2019"
 git remote set-url origin https://github.com/KevinOleniczak/WindEnergyWorkshop2019.git
-git pull
+git fetch origin
+git reset --hard origin/master
 /home/pi/WindEnergyWorkshop2019/turbine/tests/test_hardware.sh auto
 /usr/bin/python /home/pi/WindEnergyWorkshop2019/turbine/myip_oled.py $?
